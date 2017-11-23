e255d37b6a34d86cd190223088397927ff526688
yuyc <yuyc@cheyun.com>
2017-11-23 16:16:55 +0800

    ctp2.1  初版

--- ctp2.1.js
+++ ctp2.1.js
@@ -1,4 +1,4 @@
-// botvs@5b331d9a61baea5e450c473d1086d71f
+// botvs@f6d22183d0a63afec76f15f9789dbcd7
 /*backtest
 start: 2017-09-10 00:00:00
 end: 2017-11-15 00:00:00
@@ -14,7 +14,7 @@ function main() {
         var ticker = _C(exchange.GetTicker)
 
         if (records.length < 50) {
-        	return
+            return
         }
 
         var previous_bar = records[records.length-2]
@@ -73,119 +73,139 @@ function main() {
         var current_J = J[J.length-1]
         var kdj_cross = $.Cross(K, D)
 
+        // rsi
+        var rsi = TA.RSI(records,14)
+        var rsi14 = TA.RSI(records, 14)
+        var previous_rsi = rsi[rsi.length-2]
+        var current_rsi = rsi[rsi.length-1]
+        var rsi_cross = $.Cross(rsi, rsi14)
+
         // Log('openTimes=', _G(symbol+'_opentimes'), 'hold=', hold, 'ticker_price=', ticker.Last)
         // Log(ma_cross, '|ticker=', ticker.Last, 'pre_cci:cur_cci=', previous_cci, ':', current_cci)
         // Log('macd:: cross=', macd_cross, 'dif=', current_dif, 'dea=', current_dea)
 
+        /*
         // 开空
         if (mp === 0 && macd_cross >= -3  && ma_cross >= -3 && ma_cross < 0 && current_macd < 0 && (previous_bar.Close < previous_ma5)) {
 
-        	if ((previous_cci > current_cci) && current_cci < 0 && current_J > 20) {
-        		if (previous_ma20 > current_ma20) {
-        		// if (current_dif < 0 && current_dif > -10 && current_dea > -10) {
-        			Log('macd_cross=', macd_cross, ' premacd:curmacd=', previous_macd, ':', current_macd, ' predif:curdif=', previous_dif, ':', current_dif, ' dea=', previous_dea, ':',current_dea)
-	    			Log('ma_cross=', ma_cross, ' close:ma20=', previous_bar.Close, ':', previous_ma5)
-	    			Log('prema20:curma20=', previous_ma20, ':', current_ma20)
-	    			Log('ma5:ma10:ma20=', current_ma5, ':', current_ma10, ':', current_ma20)
-	    			Log('precci:curcci=', previous_cci, ':', current_cci, 'rsi=', current_rsi, 'kdj=', current_K, ':', current_D, ':', current_J, ' kdj_cross=', kdj_cross)
-	        		return -Lots
-        		// }
-        		}
-        	}
-        	
-        	// if (ma_cross < 0 && (previous_bar.Close < previous_ma20) && (current_ma10 < current_ma20)) {
-        		
-        	// 	if (previous_cci > -100 && current_cci < -100) {
-        	// 		Log('macd_cross=', macd_cross, ' macd=', current_macd)
-        	// 		Log('ma_cross=', ma_cross, ' close:ma20=', previous_bar.Close, ':', previous_ma20)
-        	// 		Log('precci:curcci=', previous_cci, ':', current_cci)
-	        // 		return -Lots
-	        // 	}
-        	// }
+            if ((previous_cci > current_cci) && current_cci < 0 && current_J > 20) {
+                if (previous_ma20 > current_ma20) {
+                    Log('macd_cross=', macd_cross, ' premacd:curmacd=', previous_macd, ':', current_macd, ' predif:curdif=', previous_dif, ':', current_dif, ' dea=', previous_dea, ':',current_dea)
+                    Log('ma_cross=', ma_cross, ' close:ma20=', previous_bar.Close, ':', previous_ma5)
+                    Log('prema20:curma20=', previous_ma20, ':', current_ma20)
+                    Log('ma5:ma10:ma20=', current_ma5, ':', current_ma10, ':', current_ma20)
+                    Log('precci:curcci=', previous_cci, ':', current_cci, 'rsi=', current_rsi, 'kdj=', current_K, ':', current_D, ':', current_J, ' kdj_cross=', kdj_cross)
+                    return -Lots
+                }
+            }
         }
 
         // 持空
         if (mp < 0) {
-        	// 空单加仓
-        	// if (ma_cross < 0 && _G(symbol+'_opentimes') < 1) {
-        	// 	_G(symbol+'_opentimes', 1)
-        	// 	return -Lots * 2
-        	// }
-        	
-       //  	if (kdj_cross > 0) {
-       //  		Log('触发止盈机制')
-    			// return 0
-       //  	}
-       //  	
-	       	if (ticker.Last < _N(hold - 3.618 * previous_atr, 0)) {
-	    		Log('触发止盈机制')
-				return 0
-	    	}
-       		
-
-        	/*
-        	// 触发止盈机制
-        	var symbol_min = _G(symbol+'_min')
-        	if (!symbol_min && ticker.Last < _N(hold - 2.618 * previous_atr, 0)) {
-        		Log('触发止盈机制')
-        		_G(symbol+'_min', ticker.Last)
-    			_G(symbol+'_moving_sell_pos', ticker.Last + 0.8 * previous_atr)
-
-    			_G(symbol+'_opentimes', 0)
-                _G(symbol+'_min', 0)
-                _G(symbol+'_moving_sell_pos', 0)
-    			return 0
-        	}
-
-        	//移动止损
-        	// 自开空单最小点位，（及目前最大盈利)
-        	// symbol_min = _G(symbol+'_min')
-        	var moving_close_sell_position = _G(symbol+'_moving_sell_pos')
-        	if (symbol_min) {
-        		// 如果有更低点，进行更新
-        		if (ticker.Last < symbol_min) {
-        			Log('更新止损位=', ticker.Last + 0.8 * previous_atr, 'min=', ticker.Last)
-        			// Log('macd_cross=', macd_cross, ' macd=', current_macd)
-        			// Log('ma_cross=', ma_cross, ' close:ma20=', previous_bar.Close, ':', previous_ma20)
-        			// Log('precci:curcci=', previous_cci, ':', current_cci)
-        			_G(symbol+'_min', ticker.Last)
-        			// 移动止损固定15个点
-        			_G(symbol+'_moving_sell_pos', ticker.Last + 0.8 * previous_atr)
-        		}
-        	}
-
-            moving_close_sell_position = _G(symbol+'_moving_sell_pos')
-            // Log('Last=', ticker.Last)
-            if (moving_close_sell_position && (ticker.Last >= moving_close_sell_position)){
-                Log('移动平仓')
-                Log('移动止损位=', moving_close_sell_position, ' premacd:cur_macd=', previous_macd, ':', current_macd, ' close:ma=', current_bar.Close, ':', current_ma5, 'price=', ticker.Last)
-                _G(symbol+'_opentimes', 0)
-                _G(symbol+'_min', 0)
-                _G(symbol+'_moving_sell_pos', 0)
+            if (ticker.Last < _N(hold - 3.618 * previous_atr, 0)) {
+                Log('触发止盈机制')
                 return 0
             }
 
-        	// 最大止盈
-        	var close_sell_position = _N(hold - 2.618 * previous_atr, 0)
-        	if (ticker.Last < close_sell_position) {
-        		_G(symbol+'_opentimes', 0)
-        		_G(symbol+'_min', 0)
-                _G(symbol+'_moving_sell_pos', 0)
-                Log('触发最大止盈::止盈点位=',close_sell_position, '当前价格=', ticker.Last)
+            // 止损
+            if (ticker.Last > _N(hold + 1.3 * previous_atr, 0)) {
+                Log('空单止损::开仓价=', hold, '止损位=', ticker.Last)
                 return 0
-        	}
-        	*/
+            }
+        }
+        */
+        
+        
+        // 开多
+        if (mp === 0) {
+            // previous_ma5 < current_ma5 && previous_ma10 < current_ma10 && 
+            // previous_ma10 > previous_ma5 && current_ma5 > current_ma10
+            // (macd_cross > 0 && previous_ma10 > previous_ma5 && current_ma5 > current_ma10
+            if (macd_cross > 0 && previous_ma10 > previous_ma5 && current_ma5 > current_ma10) {
 
-        	// 止损
-        	if (ticker.Last > _N(hold + 1.3 * previous_atr, 0)) {
-        		_G(symbol+'_opentimes', 0)
-        		_G(symbol+'_min', 0)
-                _G(symbol+'_moving_sell_pos', 0)
-        		Log('空单止损::开仓价=', hold, '止损位=', ticker.Last)
-                return 0
-        	}
+                if (previous_cci < 100 && previous_cci < current_cci && current_ma5 > current_ma10 && current_ma10 > current_ma20) {
+                    var delay_time = new Date().getTime() + 9 * 60 * 1000
+                    var open_price = ticker.Last - 8
+                    Log(symbol + ' 开多 ', ticker.Last)
+                    // 在10分周期内找出一个好的点位
+                    while (new Date().getTime() < delay_time) {
+                        var ticker_temp = _C(exchange.GetTicker)
+                        // Log('当前价格=', ticker_temp.Last)
+                        if (ticker_temp.Last <= open_price) {
+                        	var cci_ma = TA.MA(cci, 28)
+                        	var current_cci_ma = cci_ma[cci_ma.length-1]
+                            Log('macd_cross=', macd_cross, ' premacd:curmacd=', previous_macd, ':', current_macd, ' predif:curdif=', previous_dif, ':', current_dif, ' dea=', previous_dea, ':',current_dea)
+                            // Log('ma_cross=', ma_cross, ' close:ma20=', previous_bar.Close, ':', previous_ma5)
+                            Log('rsi_cross=', rsi_cross, '开仓价格=', ticker.Last)
+                            Log('prersi:currsi=', previous_rsi, ':', current_rsi, 'cci_ma=', current_cci_ma)
+                            Log('prema20:curma20=', previous_ma20, ':', current_ma20)
+                            Log('prema10:curma10=', previous_ma10, ':', current_ma10)
+                            Log('prema5:curma5=', previous_ma5, ':', current_ma5)
+                            Log('ma5:ma10:ma20=', current_ma5, ':', current_ma10, ':', current_ma20)
+                            Log('precci:curcci=', previous_cci, ':', current_cci, 'rsi=', current_rsi, 'kdj=', current_K, ':', current_D, ':', current_J, ' kdj_cross=', kdj_cross)
+                            return Lots
+                        }
+                    }
+
+                    return Lots
+                    
+                }
+            }
+            /*
+            if (macd_cross > 0) {
+                if (previous_bar.Close > previous_ma5 && previous_ma20 < current_ma20) {
+                    if (previous_ma5 < current_ma5 && previous_ma10 < current_ma10 && previous_ma10 > previous_ma5 && current_ma5 > current_ma10) {
+                        if (previous_cci < current_cci) {
+                            Log('macd_cross=', macd_cross, ' premacd:curmacd=', previous_macd, ':', current_macd, ' predif:curdif=', previous_dif, ':', current_dif, ' dea=', previous_dea, ':',current_dea)
+                            // Log('ma_cross=', ma_cross, ' close:ma20=', previous_bar.Close, ':', previous_ma5)
+                            Log('prersi:currsi=', previous_rsi, ':', current_rsi)
+                            Log('prema20:curma20=', previous_ma20, ':', current_ma20)
+                            Log('prema10:curma10=', previous_ma10, ':', current_ma10)
+                            Log('prema5:curma5=', previous_ma5, ':', current_ma5)
+                            Log('ma5:ma10:ma20=', current_ma5, ':', current_ma10, ':', current_ma20)
+                            Log('precci:curcci=', previous_cci, ':', current_cci, 'rsi=', current_rsi, 'kdj=', current_K, ':', current_D, ':', current_J, ' kdj_cross=', kdj_cross)
+                            return Lots
+                        }
+                    }
+                }
+            }*/
+            
+            
         }
 
+        //持多
+        if (mp > 0) {
+            //////////////////////////////////////////////////////////////////////////////
+            //特殊转向也分向上转向和向下转向两种。若当日最高价高于前一日的最高价，且最低价低于前一日的最低价，而收盘价高于上一日收盘价，便构成特殊向上转向信号。 //
+            //反之，若当日最高价低于上日最高价，且最低价高于上一日的最低价，而收盘价低于上一日收盘价，便构成特殊向下转向信号。
+            //////////////////////////////////////////////////////////////////////////////
+            var ma5_records = exchange.GetRecords(PERIOD_M5)
+            var previous_ma5_bar = ma5_records[ma5_records.length-2]
+            var current_ma5_bar = ma5_records[ma5_records.length-1]
+
+            var ma5_cci = talib.CCI(ma5_records, cci_windows)
+            var previous_ma5_cci = ma5_cci[ma5_cci.length-2]
+            var current_ma5_cci = ma5_cci[ma5_cci.length-1]
+
+            if (ticker.Last > hold && current_ma5_bar.High < previous_ma5_bar.High && current_ma5_bar.Low > previous_ma5_bar.Low && current_ma5_bar.Close <= previous_ma5_bar.Close) {
+                Log('previous_ma5_cci=', previous_ma5_cci, 'current_ma5_cci=', current_ma5_cci)
+                if (previous_ma5_cci > 180 && previous_ma5_cci > current_ma5_cci) {
+                	Log('kdj=', current_K, ':', current_D, ':', current_J)
+                    // Log('ma5 bar=', current_ma5_bar)
+                    return 0
+                }
+            }
+
+
+            // Log('KDJ=', current_D, ':', current_J, 'price=', ticker.Last, 'low=', current_bar.Low, 'close=', current_bar.Close)
+            // 止损
+            if (ticker.Last < _N(hold - 1.3 * previous_atr, 0)) {
+                Log('多单止损::开仓价=', hold, '止损位=', ticker.Last)
+                return 0
+            }
+        }
+        
+        
 
 
     })
